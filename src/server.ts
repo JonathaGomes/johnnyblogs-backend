a9a951f39e0415401200a5a4da9d3f42050ad386
Jonatha Gomes <jonatha.dev.js@gmail.com>
2021-12-10 00:29:38 -0300

    :sparkles: rename project

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,6 @@
 import { app } from "./app";
 
-const logo = `
- ██████  ██   ██  █████  ██    ██ ██████  ███████ ██    ██ 
-██    ██ ██  ██  ██   ██  ██  ██  ██   ██ ██      ██    ██ 
-██    ██ █████   ███████   ████   ██   ██ █████   ██    ██ 
-██    ██ ██  ██  ██   ██    ██    ██   ██ ██       ██  ██  
- ██████  ██   ██ ██   ██    ██    ██████  ███████   ████   
-powered by: Jonatha Gomes
-`;
-
 app.listen(process.env.PORT || 3333, () => {
-  console.log(logo);
   if (process.env.NODE_ENV == "dev") {
     console.log("Server is running in development mode");
   } else {
